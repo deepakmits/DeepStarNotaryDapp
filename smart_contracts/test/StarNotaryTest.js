3d3a2f15f5f3af0d1954354580b92daacb1ca3e3
deepakmits <deepakmits@yahoo.com>
2018-11-05 22:56:54 +0530

    some more tests

--- smart_contracts/test/StarNotaryTest.js
+++ smart_contracts/test/StarNotaryTest.js
@@ -12,5 +12,11 @@ contract('StarNotary' , accounts => {
         it('has correct name', async () => {
             assert.equal(await contractInstance.starName(), 'Awesome Star')
         })
+
+        it('star cab be claimed', async () => {
+            assert.equal(await contractInstance.starOwner(), 0)
+            await contractInstance.claimStar({from : owner})
+            assert.equal(await contractInstance.starOwner(), owner)
+        })
     })
 })
