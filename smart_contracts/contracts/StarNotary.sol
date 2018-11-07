397a43e1f9f4a0fe47eab33f38729fb78c71bf46
deepakmits <deepakmits@yahoo.com>
2018-11-07 17:43:35 +0530

    more tests

--- smart_contracts/contracts/StarNotary.sol
+++ smart_contracts/contracts/StarNotary.sol
@@ -1,19 +1,94 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.23;
 
-contract StarNotary {
-    
-    string public starName;
-    address public starOwner;
-    
-    event starClaimed(address owner);
-    
-    constructor() public {
-        starName = "Awesome Star";
+import "./ERC721Token.sol";
+
+contract StarNotary is ERC721Token { 
+
+    struct Star { 
+        string name;
+        string dec;
+        string mag;
+        string cent;
+        string story;    
+    }
+
+    //Token ID <=> Star Id
+    //tokenid to Star object
+    mapping(uint256 => Star) public tokenIdToStarInfo; 
+    //starId to price
+    mapping(uint256 => uint256) public starsForSale;
+    //mapping storing encoded string of star cordinates to true when created.
+    mapping(string => bool) private existingStars;
+    //starAdded event
+    event starAdded(address owner);
+
+    function createStar(string _name, string _cent, string _dec, string _mag, string story, uint256 _tokenId) public { 
+
+        require(!checkIfStarExist(_cent,_dec,_mag),"Star with given coordinates already exists");
+        Star memory newStar = Star(_name,_cent,_dec,_mag,story);
+        tokenIdToStarInfo[_tokenId] = newStar;
+        //adding to encoded maps
+        existingStars[string(abi.encode(_cent,_dec,_mag))] = true;
+        ERC721Token.mint(_tokenId);
+        emit starAdded(msg.sender);
+    }
+
+    function checkIfStarExist(string _cent, string _dec, string _mag) public view returns (bool){
+        return existingStars[string(abi.encode(_cent,_dec,_mag))];
+    }
+
+
+    /* struct Star { 
+        string name;
     }
     
-    function claimStar() public {
-        starOwner = msg.sender;
-        emit starClaimed(msg.sender);
+    mapping(uint256 => Star) public tokenIdToStarInfo;
+
+    mapping(uint256 => uint256) public starsForSale;
+
+    function createStar(string _name, uint256 _tokenId) public { 
+        Star memory newStar = Star(_name);
+
+        tokenIdToStarInfo[_tokenId] = newStar;
+
+        ERC721Token.mint(_tokenId);
+    } */
+
+    function putStarUpForSale(uint256 _tokenId, uint256 _price) public { 
+        require(this.ownerOf(_tokenId) == msg.sender);
+
+        starsForSale[_tokenId] = _price;
+    }
+
+    function buyStar(uint256 _tokenId) public payable { 
+        require(starsForSale[_tokenId] > 0);
+
+        uint256 starCost = starsForSale[_tokenId];
+        address starOwner = this.ownerOf(_tokenId);
+
+        require(msg.value >= starCost);
+
+        clearPreviousStarState(_tokenId);
+
+        transferFromHelper(starOwner, msg.sender, _tokenId);
+
+        if(msg.value > starCost) { 
+            msg.sender.transfer(msg.value - starCost);
+        }
+
+        starOwner.transfer(starCost);
+    }
+
+    function clearPreviousStarState(uint256 _tokenId) private {
+        //clear approvals 
+        tokenToApproved[_tokenId] = address(0);
+
+        //clear being on sale 
+        starsForSale[_tokenId] = 0;
+    }
+
+    function tokenIdToStarInfo(uint256 tokenId) public view returns(string, string, string, string, string) {
+        return (tokenIdToStarInfo[tokenId].name, tokenIdToStarInfo[tokenId].story, tokenIdToStarInfo[tokenId].cent, 
+        tokenIdToStarInfo[tokenId].dec, tokenIdToStarInfo[tokenId].mag);
     }
 }
-
--- smart_contracts/test/StarNotaryTest.js
+++ smart_contracts/test/StarNotaryTest.js
@@ -1,41 +1,148 @@
-const starDefinition = artifacts.require('StarNotary')
+const StarNotary = artifacts.require('StarNotary')
 
-contract('StarNotary' , accounts => {
-    var owner = accounts[0]
-    var contractInstance
+contract('StarNotary', accounts => { 
 
-    beforeEach(async function(){
-        contractInstance = await starDefinition.new({from: owner})            
+    beforeEach(async function() { 
+        this.contract = await StarNotary.new({from: accounts[0]})
     })
 
-    describe('StarNotary Basics' , ()=>{
-        it('has correct name', async () => {
-            assert.equal(await contractInstance.starName(), 'Awesome Star')
+    //Creation of Star 
+    describe('can create a star', () => { 
+        let tokenId1 = 1
+        it('can create a star and get its name', async function () { 
+            await this.contract.createStar('Deep star1', 'ra_032.155', 'dec_121.874', 'mag_245.978', 'My Deep Star Story 1', tokenId1, {from: accounts[0]})
+            const star1 = await this.contract.tokenIdToStarInfo(tokenId1);
+            assert.equal(star1[0], 'Deep star1');
         })
 
-        it('star cab be claimed', async () => {
-            assert.equal(await contractInstance.starOwner(), 0)
-            await contractInstance.claimStar({from : owner})
-            assert.equal(await contractInstance.starOwner(), owner)
+        let tokenId2 = 2
+        it('can create a second star also and get its name too', async function () { 
+            await this.contract.createStar('Deep star2', 'ra_033.156', 'dec_122.874', 'mag_246.97', 'My Deep Star Story 2', tokenId2, {from: accounts[0]})
+            let star2 = await this.contract.tokenIdToStarInfo(tokenId2);
+            assert.equal(star2[0], 'Deep star2');
         })
     })
 
-
-    describe('star can change owners', ()=>{
-        beforeEach(async ()=>{
-            assert.equal(await contractInstance.starOwner(),0)
-            await contractInstance.claimStar({from : owner})
+    describe('check if start already exists', () => {
+        let tokenId1 = 1
+        let user1 = accounts[1]
+        beforeEach(async function () {
+            await this.contract.createStar('Deep star1', 'ra_032.155', 'dec_121.874', 'mag_245.978', 'My Deep Star Story 1', tokenId1, {from: user1})
         })
 
-        it('can be claimed by second user', async () => {
-            var secondUser = accounts[1]
-            await contractInstance.claimStar({from: secondUser})
-
-            assert.equal(await contractInstance.starOwner(), secondUser)
-        })
-
-
+        it('check if start exists', async function () {
+            assert.equal(await this.contract.checkIfStarExist('ra_032.155','dec_121.874','mag_245.978'),true) ;
+        }) 
     })
 
-    
+
+    describe('buying and selling stars', () => { 
+
+        let user1 = accounts[1]
+        let user2 = accounts[2]
+
+        let starId = 1
+        let starPrice = web3.toWei(.05, "ether")
+
+        beforeEach(async function () {
+            await this.contract.createStar('Deep star1', 'ra_032.155', 'dec_121.874', 'mag_245.978', 'My Deep Star Story 1', starId, {from: user1})
+        })
+
+        describe('user1 can sell a star', () => { 
+            //star is added to starsforsale mapping.
+            it('user1 can put up their star for sale', async function () { 
+                await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
+            
+                assert.equal(await this.contract.starsForSale(starId), starPrice)
+            })
+            //funds are increased by starprice of star1 after selling it for user 1
+            it('user1 gets the funds after selling a star', async function () { 
+                
+                await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
+
+                let balanceOfUser1BeforeTransaction = web3.eth.getBalance(user1)
+                await this.contract.buyStar(starId, {from: user2, value: starPrice})
+                let balanceOfUser1AfterTransaction = web3.eth.getBalance(user1)
+
+                assert.equal(balanceOfUser1BeforeTransaction.add(starPrice).toNumber(), 
+                            balanceOfUser1AfterTransaction.toNumber())
+            })
+        })
+
+        describe('user2 can buy a star that was put up for sale', () => { 
+            beforeEach(async function () { 
+                await this.contract.putStarUpForSale(starId, starPrice, {from: user1})
+            })
+            //buying successful
+            it('user2 is the owner of the star after they buy it', async function () { 
+                await this.contract.buyStar(starId, {from: user2, value: starPrice})
+                //check new owner of star
+                assert.equal(await this.contract.ownerOf(starId), user2)
+            })
+            //change is returned to new owners account
+            it('user2 correctly has their balance changed', async function () { 
+                let overpaidAmount = web3.toWei(.07, 'ether')
+
+                const balanceOfUser2BeforeTransaction = web3.eth.getBalance(user2)
+                await this.contract.buyStar(starId, {from: user2, value: overpaidAmount, gasPrice:0})
+                const balanceAfterUser2BuysStar = web3.eth.getBalance(user2)
+
+                assert.equal(balanceOfUser2BeforeTransaction.sub(balanceAfterUser2BuysStar), starPrice)
+            })
+        })
+    })
+
+    //approval to other user
+    describe('user1 giving approval to transfer star to user2', () => {
+
+        let user1 = accounts[1]
+        let user2 = accounts[2]
+
+        let starId = 1
+
+        beforeEach(async function () {
+            await this.contract.createStar('Deep star1', 'ra_032.155', 'dec_121.874', 'mag_245.978', 'My Deep Star Story 1', starId, {from: user1})
+            tx = await this.contract.approve(user2, starId, {from: user1});   
+        })
+
+        //user2 is approved
+        it('user2 is new approved for tokenid1', async function() {
+            assert.equal(await this.contract.getApproved(starId), user2);
+        })
+
+        //user2 can executed transfer
+        it('user2 can transfer the tokenId1', async function(){
+            await this.contract.transferFrom(user1, user2, starId, {from: user2});
+            assert.equal(await this.contract.ownerOf(starId), user2);
+        })
+
+        //correct event is emitted
+        it('emitted event is correct', async () => {
+            assert.equal(tx.logs[0].event, 'Approval');
+        })
+    })
+
+   
+    //approval to opeartor/exchange
+    describe('user1 giving approval to operator/exchange to transfer', () => {
+        let user1 = accounts[1]
+        let operator = accounts[3]
+
+        let starId = 1;
+        let tx ;
+        beforeEach(async function() { 
+            await this.contract.createStar('Deep star1', 'ra_032.155', 'dec_121.874', 'mag_245.978', 'My Deep Star Story 1', starId, {from: user1});
+            tx = await this.contract.setApprovalForAll(operator, true, {from: user1});  
+        })
+
+        //operator account3 is approved
+        it('operator is newly approved', async function() {
+            assert.equal(await this.contract.isApprovedForAll(user1, operator), true);
+        })
+
+        //correct event is emitted
+        it('emitted event is correct', async function() {
+            assert.equal(tx.logs[0].event, 'ApprovalForAll');
+        })
+    })
 })
